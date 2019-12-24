9a0132581b9de564c21b21c9a7c1e9bcfc7b6f37
xull_ling <xull_ling@163.com>
2019-12-24 18:52:18 +0800

    我的页面

--- lib/page/MinePage.dart
+++ lib/page/MinePage.dart
@@ -22,19 +22,103 @@ class MinePageState extends State<MinePage>{
 //          backgroundColor: Color.fromARGB(255, 199, 136, 213),
 //          centerTitle: true,
 //        ),
-        body: Column(
+          body: Container(
+            margin:  EdgeInsets.fromLTRB(10,50,0,0),
+            child: Column(
+              children: <Widget>[
+                Row(children: <Widget>[
+                  CircleAvatar(
+                    radius: 36.0,
+                    backgroundImage: AssetImage(Utils.getImgPath2("ic_avatar")),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    child:Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "比格",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                          child:
+                            Text(
+                              "电话号码：15313729066",
+                              style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Color.fromARGB(100, 178, 178, 178)
+                              ),
 
-          children: <Widget>[
-            CircleAvatar(
-              radius: 36.0,
-              backgroundImage: AssetImage(Utils.getImgPath2("ali_connors")),
+                            )
+                        ),
+
+                      ],
+                    )
+                  )
+                  ,
+
+
+                ],)
+                ,
+                getText("设备管理"),
+                Divider(height: 1.0,color: Colors.blueGrey),
+                getText("提醒设置"),
+                Divider(height: 1.0,color: Colors.blueGrey),
+                getText("服务条款"),
+                Divider(height: 1.0,color: Colors.blueGrey),
+                getText("意见反馈"),
+                Divider(height: 1.0,color: Colors.blueGrey),
+                getText("关于我们"),
+                Divider(height: 1.0,color: Colors.blueGrey),
+
+              ],
             ),
-          ],
-        ),
+          ),
+//        body: Column(
+//          children: <Widget>[
+//            Row(children: <Widget>[
+//              CircleAvatar(
+//                radius: 36.0,
+//                backgroundImage: AssetImage(Utils.getImgPath2("ic_avatar")),
+//              ),
+//
+//            ],)
+//
+//          ],
+//        ),
       ),
 
 
     );
   }
+  Container getText(String text){
+    return Container(
+      padding: EdgeInsets.fromLTRB(10,0,10,0),
+      height: 50.0,
+      child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Text(
+                text,
+                style: TextStyle(
+                    fontSize: 16.0,
+                ),
+              ),
+            )
+            ,
+            Image(
+              image: AssetImage(Utils.getImgPath2("ic_arraw")),
+            )
+
+          ],
+      ),
+    );
+  }
+
+
 
 }
